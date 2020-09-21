60f77ab391da755e101b34df47efeb2d9ea7816e
pepe <pepe@DESKTOP-JT5G5K0>
2020-09-21 18:46:14 -0400

    fixed issue, added example

---
+++ README.md
@@ -0,0 +1,34 @@
+# img2pdf
+
+This script will convert all the images (jpg) from a given directory to a pdf file.
+
+This is specially useful to create sliders (like instagram slider) for Linkedin posts 
+
+## Example
+
+![example](link)
+
+## Install dependencies 
+
+```
+pip install -r requirements.txt #(Python 2)
+```
+
+```
+ pip3 install -r requirements.txt #(Python 3)
+```
+
+## Run the script
+
+To convert all the images in the same directory where converter.py is located, run the following command 
+
+```
+python converter.py 
+```
+
+To convert all the images in given directory, run the following command 
+
+```
+python converter.py my_directory
+```
+
--- converter.py
+++ converter.py
@@ -2,18 +2,26 @@ import os
 import img2pdf # pip install img2pdf
 import sys
 
-current_path = str(os.path.abspath(os.getcwd())) + '\\'
+current_path = os.path.abspath(os.getcwd())
 path = '.'
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        path = current_path + str(sys.argv[1])
+        path = os.path.join(current_path, sys.argv[1])
     else:
         print('If there is not parameter, the script will try to get the files form the current working directory')
 
-list_of_images = [i for i in os.listdir(path) if i.endswith(".jpg")]
 
-with open("output.pdf", "wb") as f:
+# list_of_images = [i for i in os.listdir(path) if i.endswith(".jpg")]
+list_of_images = []
+for file_path in os.listdir(path):
+        if file_path.endswith(".jpg"):
+            list_of_images.append(os.path.join(path, file_path))
+        else:
+            pass
+
+
+with open("output.pdf", "wb+") as f:
     f.write(img2pdf.convert(list_of_images))
 
 print('File created!')
